27db5fc2ae6fcd0153daad2df84dc7d2a51d7546
BrunoBerger <bruno.berger@stud.hshl.de>
2022-01-01 19:08:24 +0100

    added base bot info to /info

--- commands/info.js
+++ commands/info.js
@@ -1,11 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Client } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('Print many information'),
 	async execute(interaction) {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		var client = interaction.client;
+		var uptime = new Date(client.uptime).toISOString().substr(11, 8);
+		var embed = new MessageEmbed()
+			.setColor('#e68447')
+			.setTitle("Bot and server info")
+			.setThumbnail(client.user.displayAvatarURL())
+			.addFields(
+				{ name: 'Bot uptime:', value: uptime, inline: true },
+				{ name: "Bot created at:", value: client.user.createdAt.toDateString(), inline: false }
+			)
+		await interaction.reply({ embeds: [embed] , ephemeral: true});
 	},
 };
